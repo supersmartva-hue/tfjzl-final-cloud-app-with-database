97e818a5b1c4c7545a7aae4e13ff76c0ace0b057
Nadia Mahak <supersmartva@gmail.com>
2026-03-09 13:36:50 +0500

    Register Question and Choice models in admin

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,30 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
-# <HINT> Register QuestionInline and ChoiceInline classes here
-from .models import Question, Choice, Submission
-
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 
-# Register your models here.
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'course', 'grade')
 
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ('title', 'course')
 
 
-# <HINT> Register Question and Choice models here
-
-admin.site.register(Course, CourseAdmin)
+admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
