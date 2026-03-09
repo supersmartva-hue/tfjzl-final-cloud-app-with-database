d01ead955e7302e5decd652abdb428cb707f5281
Nadia Mahak <supersmartva@gmail.com>
2026-03-09 13:26:41 +0500

    Add Question and Choice models for quiz functionality

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,3 +101,30 @@ class Enrollment(models.Model):
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    grade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.question_text
+
+    # method to calculate if the learner gets the score
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        return all_answers == selected_correct
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
